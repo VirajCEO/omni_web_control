ae81189f84ec16664f071952e43f287b0897629c
VirajCEO <work.aloxen@gmail.com>
2024-01-01 04:04:10 +0530

    change

--- bot.py
+++ bot.py
@@ -147,7 +147,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, host=ip)
+    app.run(debug=True, host='0.0.0.0')
 
-if __name__ == '__main__':
-    app.run(debug=True, host=ip)
